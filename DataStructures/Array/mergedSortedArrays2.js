14617e557609478ac7b8d65bffe77b4c518c3ea6
guisalmeida <guisalmeida.dev@gmail.com>
2024-08-24 21:11:39 -0300

    refactor: merged sorted arrays file

--- DataStructures/Array/mergedSortedArrays2.js
+++ DataStructures/Array/mergedSortedArrays2.js
@@ -1,15 +1,7 @@
 
-
-
-
 function mergeSortedArrays(arr1, arr2) {
     const sortedArray = [...arr1, ...arr2];
-
     return sortedArray.sort((a,b)=> a - b);
-
-
-
-
 }
 
 console.log(mergeSortedArrays([0,3,4,31], [4,6,30]));
